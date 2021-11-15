03c6a22cc5ee7d0be8a6674b04fa53714f4681ca
Walker <3987+walker@users.noreply.github.com>
2021-11-14 19:48:27 -0600

    Documented Case Admin methods. Tagged 0.0.21

--- dist/case_admin.d.ts
+++ dist/case_admin.d.ts
@@ -34,7 +34,7 @@ export declare class CaseAdmin {
     /**
      * Add Case Type
      *
-     * @category Types
+     * @category Types & SubTypes
      * @param {string} caseTypeName - The case type name
      * @param {string} caseTypeDesciption - The case type description
      * @param {Object} options - The other options for the Case Type
@@ -44,7 +44,7 @@ export declare class CaseAdmin {
     /**
      * Update Case Type
      *
-     * @category Types
+     * @category Types & SubTypes
      * @param {number} caseTypeId - The case Type ID
      * @param {Object} options - The other options for the Case Type
      * @return {Object} Returns Promise that represents an object describing the updated case type
@@ -53,14 +53,14 @@ export declare class CaseAdmin {
     /**
      * Get Case Types
      *
-     * @category Types
+     * @category Types & SubTypes
      * @return {Object} Returns Promise that represents a collection of CaseTypeItems
      */
     getCaseTypes(): Promise<unknown>;
     /**
      * Search for CaseTypes. Include at least one of the search fields. A logical 'and' operation is applied for multiple search fields.
      *
-     * @category Types
+     * @category Types & SubTypes
      * @param {Object} filters - The parameter(s) to search by (CaseType, CaseTypeDesc, CaseTypeId)
      * @return {Object} Returns Promise that represents an Array of CaseTypeIDs
      */
@@ -68,7 +68,7 @@ export declare class CaseAdmin {
     /**
      * Search for CaseTypes. Include at least one of the search fields. A logical 'and' operation is applied for multiple search fields.
      *
-     * @category Types
+     * @category Types & SubTypes
      * @param {Object} filters - The parameter(s) to search by (CaseType, CaseTypeDesc, CaseTypeId)
      * @return {Object} Returns Promise that represents a collection of CaseTypeItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=CaseTypeItem
      */
@@ -76,7 +76,7 @@ export declare class CaseAdmin {
     /**
      * Add SubType
      *
-     * @category SubTypes
+     * @category Types & SubTypes
      * @param {string} subTypeName - The SubTypeId
      * @param {string} subTypeDescription - The SubTypeId
      * @param {Object} options - Other SubType options.
@@ -86,7 +86,7 @@ export declare class CaseAdmin {
     /**
      * Update SubType
      *
-     * @category SubTypes
+     * @category Types & SubTypes
      * @param {number} subTypeId - The SubTypeId
      * @param {Object} options - The case Object ID
      * @return {Object} Returns Promise that represents an object describing the updated SubTypeItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=SubTypeItem
@@ -95,27 +95,10 @@ export declare class CaseAdmin {
     /**
      * Get SubTypes
      *
-     * @category SubTypes
+     * @category Types & SubTypes
      * @return {Object} Returns Promise that represents a collection of SubTypeItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=SubTypeItem
      */
     getSubtypes(): Promise<unknown>;
-    /**
-     * Add Department
-     *
-     * @category Departments
-     * @param {string} departmentCode - The SubTypeId
-     * @param {string} dpartmentName - The SubTypeId
-     * @param {Object} options - Other SubType options.
-     * @return {Object} Returns Promise that represents an object describing the added DepartmentItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DepartmentItem
-     */
-    addDepartment(departmentCode: string, dpartmentName: string, options?: Object): Promise<unknown>;
-    /**
-     * Get Departments
-     *
-     * @category Departments
-     * @return {Object} Returns Promise that represents a collection of SubTypeItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=SubTypeItem
-     */
-    getDepartments(): Promise<unknown>;
     /**
      * Get all Fees
      *
@@ -147,7 +130,242 @@ export declare class CaseAdmin {
      * Add an Expiration type
      *
      * @category Expirations
-     * @return {Object} Returns Promise that represents an object describing the newly-added ExpirationTypeItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=ExpirationTypeItem
+     * @param {Object} options - Must specify OrgId and ExpirationDescType attributes. See: /{subdirectory}/apidocs/#/service-info/Pll/ExpirationType
+     * @return {Object} Returns Promise that represents an object describing the newly-added ExpirationTypeItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=ExpirationTypeItem
      */
     addExpiration(options?: Object): Promise<unknown>;
+    /**
+     * Get all Deposits
+     *
+     * @category Deposits
+     * @return {Object} Returns Promise that represents a collection of DepositItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DepositItemBase
+     */
+    getDeposits(): Promise<unknown>;
+    /**
+     * Search Deposits. Include at least one of the search fields. A logical 'and' operation is applied for multiple search fields.
+     *
+     * @param {Object} filters - The parameter(s) to search by (AccountCode, DepositCode, DepositDesc, DepositId, DepositTypeId).
+     * @return {Object} Returns Promise that represents an Array of FeeSetupIDs
+     */
+    searchDeposits(filters: {
+        AccountCode?: string;
+        DepositCode?: string;
+        DepositDesc?: string;
+        DepositId?: number;
+        DepositTypeId?: number;
+    }): Promise<unknown>;
+    /**
+     * Get all Disciplines
+     *
+     * @category Disciplines
+     * @return {Object} Returns Promise that represents a collection of DisciplineItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DisciplineItem
+     */
+    getDisciplines(): Promise<unknown>;
+    /**
+     * Add Department
+     *
+     * @category Departments & Divisions
+     * @param {string} departmentCode - The SubTypeId
+     * @param {string} departmentName - The SubTypeId
+     * @param {Object} options - Other SubType options.
+     * @return {Object} Returns Promise that represents an object describing the added DepartmentItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DepartmentItem
+     */
+    addDepartment(departmentCode: string, departmentName: string, options?: Object): Promise<unknown>;
+    /**
+     * Get Departments
+     *
+     * @category Departments & Divisions
+     * @return {Object} Returns Promise that represents a collection of SubTypeItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DepartmentItem
+     */
+    getDepartments(): Promise<unknown>;
+    /**
+     * Add Division
+     *
+     * @category Departments & Divisions
+     * @return {Object} Returns Promise that represents an object describing the newly-added DivisionItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DivisionItem
+     */
+    addDivision(departmentId: number, divisionName: string, options?: Object): Promise<unknown>;
+    /**
+     * Get all Divisions
+     *
+     * @category Departments & Divisions
+     * @return {Object} Returns Promise that represents a collection of DivisionItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=DivisionItem
+     */
+    getDivisions(): Promise<unknown>;
+    /**
+     * Add Holiday
+     *
+     * @category Holidays
+     * @return {Object} Returns Promise that represents an object describing the newly-added DivisionItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=HolidaysItem
+     */
+    addHoliday(holidayDate: Date, holidayDesc: string, halfDayFlag?: string): Promise<unknown>;
+    /**
+     * Delete Holiday
+     *
+     * @category Holidays
+     * @param {number} holidayId - ID of Holiday to delete
+     * @return {Object} Returns Promise that represents an object describing the newly-added HolidaysItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=HolidaysItem
+     */
+    deleteHoliday(holidayId: number): Promise<unknown>;
+    /**
+     * Get all Holidays
+     *
+     * @category Holidays
+     * @return {Object} Returns Promise that represents a collection of HolidaysItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=HolidaysItem
+     */
+    getHolidays(): Promise<unknown>;
+    /**
+     * Add Inspection Time Block
+     *
+     * @category Inspections
+     * @param {string} labelText - Label for the time block
+     * @param {string} startTime - Start time in 24 hour format (HH:mm)
+     * @param {string} endTime - End time in 24 hour format (HH:mm)
+     * @param {Object} inspTimeBlocksDetails - See: /{subdirectory}/apidocs/#/data-type-info;dataType=InspTimeBlocksDetailItem
+     * @return {Object} Returns Promise that represents an object describing the newly-added InspectionTimeBlocksItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=InspectionTimeBlocksItem
+     */
+    addInspectionTimeBlock(labelText: string, startTime: string, endTime: string, inspTimeBlocksDetails?: Object): Promise<unknown>;
+    /**
+     * Get all Inspection Time Blocks
+     *
+     * @category Inspections
+     * @return {Object} Returns Promise that represents a collection of InspectionTimeBlocksItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=InspectionTimeBlocksItem
+     */
+    getInspectionTimeBlocks(): Promise<unknown>;
+    /**
+     * Get all NotificationCases
+     *
+     * @category Notifications
+     * @return {Object} Returns Promise that represents a collection of NotificationCaseItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NotificationCaseItem
+     */
+    getNotificationCases(): Promise<unknown>;
+    /**
+     * Get all Notification Types
+     *
+     * @category Notifications
+     * @return {Object} Returns Promise that represents a collection of NotificationCaseItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NotificationTypeItem
+     */
+    getNotificationTypes(): Promise<unknown>;
+    /**
+     * Add Numbering Group
+     *
+     * @category NumberingGroups
+     * @param {number} options - attributes or update
+     * @return {Object} Returns Promise that represents an object describing the newly-added NumberingGroupItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NumberingGroupItem
+     */
+    addNumberingGroup(options: Object): Promise<unknown>;
+    /**
+     * Update Numbering Group
+     *
+     * @category NumberingGroups
+     * @param {number} numberingGroupId - ID of NumberGroup to update
+     * @param {number} options - updates attributes
+     * @return {Object} Returns Promise that represents an object describing the newly-added HolidaysItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NumberingGroupItem
+     */
+    updateNumberingGroup(numberingGroupId: number, options?: Object): Promise<unknown>;
+    /**
+     * Get all Numbering Groups
+     *
+     * @category NumberingGroups
+     * @return {Object} Returns Promise that represents a collection of NumberingGroupItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NumberingGroupItem
+     */
+    getNumberingGroups(): Promise<unknown>;
+    /**
+     * Add Person
+     *
+     * @category People
+     * @param {string} name - Name of person
+     * @param {Object} options - attributes for new person
+     * @return {Object} Returns Promise that represents an object describing the newly-added NumberingGroupItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=NumberingGroupItem
+     */
+    addPerson(name: string, options: Object): Promise<unknown>;
+    /**
+     * Get all People
+     *
+     * @category People
+     * @return {Object} Returns Promise that represents a collection of PeopleItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=PeopleItem
+     */
+    getPeople(): Promise<unknown>;
+    /**
+     * Add People Role
+     *
+     * @category People
+     * @param {string} name - Name of person
+     * @param {Object} options - attributes for new person. See: /{subdirectory}/apidocs/#/service-info/Pll/PeopleRole
+     * @return {Object} Returns Promise that represents an object describing the newly-added PeopleRoleItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=PeopleRoleItem
+     */
+    addPeopleRole(roleCode: string, options: Object): Promise<unknown>;
+    /**
+     * Get all PeopleRoles
+     *
+     * @category People
+     * @return {Object} Returns Promise that represents a collection of PeopleRoleItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=PeopleRoleItem
+     */
+    getPeopleRoles(): Promise<unknown>;
+    /**
+     * Add TaskResultDetail
+     *
+     * @category TaskResults
+     * @param {Object} options - attributes for new TaskResultDetail. See: /{subdirectory}/apidocs/#/service-info/Pll/TaskResultDetail
+     * @return {Object} Returns Promise that represents an object describing the newly-added TaskResultDetailItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=TaskResultDetailItem
+     */
+    addTaskResultDetail(options: Object): Promise<unknown>;
+    /**
+     * Update TaskResultDetail
+     *
+     * @category TaskResults
+     * @param {string} resultDetailId - Result Detail ID
+     * @param {Object} options - attributes for new person. See: /{subdirectory}/apidocs/#/service-info/Pll/TaskResultDetail
+     * @return {Object} Returns Promise that represents an object describing the newly-added TaskResultDetailItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=TaskResultDetailItem
+     */
+    updateTaskResultDetail(resultDetailId: number, options: Object): Promise<unknown>;
+    /**
+     * Get all TaskResultDetails
+     *
+     * @category TaskResults
+     * @return {Object} Returns Promise that represents a collection of PeopleRoleItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=PeopleRoleItem
+     */
+    getTaskResultDetails(): Promise<unknown>;
+    /**
+     * Add TaskResultFeeInsert
+     *
+     * @category TaskResults
+     * @param {Object} options - attributes for new TaskResultFeeInsert. See: /{subdirectory}/apidocs/#/service-info/Pll/TaskResultFeeInsert
+     * @return {Object} Returns Promise that represents an object describing the newly-added TaskResultFeeInsertItem. See: /{subdirectory}/apidocs/#/data-type-info;dataType=TaskResultFeeInsertItem
+     */
+    addTaskResultFeeInsert(options: Object): Promise<unknown>;
+    /**
+     * Delete TaskResultFeeInsert
+     *
+     * @category TaskResults
+     * @param {number} taskResultFeeInsertId - ID for TaskResultFeeInsert to be deleted.
+     * @return {Object} Returns Promise that represents the ID Number of the deleted TaskResultFeeInsertItem.
+     */
+    deleteTaskResultFeeInsert(taskResultFeeInsertId: number): Promise<unknown>;
+    /**
+     * Delete TaskResultFeeInsert(s) by Task ID
+     *
+     * @category TaskResults
+     * @param {number} taskId - ID for Task parent of TaskResultFeeInsert(s) to be deleted.
+     * @return {Object} Returns Promise that represents the ID Number of the Task parent of the deleted TaskResultFeeInsertItem(s).
+     */
+    deleteTaskResultFeeInsertByTaskId(taskId: number): Promise<unknown>;
+    /**
+     * Update TaskResultFeeInsert
+     *
+     * @category TaskResults
+     * @param {number} taskResultFeeInsertId - ID for TaskResultFeeInsert to be updated.
+     * @param {number} taskId - Updated TaskID
+     * @param {number} taskResultId - Updated TaskResultId
+     * @param {number} [feeSetupId] - Fee setup ID update for TaskResultFeeInsert
+     * @return {Object} Returns Promise that represents the ID Number of the updated TaskResultFeeInsertItem.
+     */
+    updateTaskResultFeeInsert(taskResultFeeInsertId: number, taskId: number, taskResultId: number, feeSetupId?: number): Promise<unknown>;
+    /**
+     * Get all TaskResultFeeInsert
+     *
+     * @category TaskResults
+     * @return {Object} Returns Promise that represents a collection of TaskResultFeeInsertItems. See: /{subdirectory}/apidocs/#/data-type-info;dataType=TaskResultFeeInsertItem
+     */
+    getTaskResultFeeInsert(): Promise<unknown>;
 }
