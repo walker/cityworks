0e1b9832ffa2c894b1177a3cf5b2a2d6d1539869
Walker <3987+walker@users.noreply.github.com>
2021-11-21 21:35:51 -0600

    Many of the case financial tests. 4 stubbed

--- test/06.caseFinancialTest.js
+++ test/06.caseFinancialTest.js
@@ -63,8 +63,8 @@ describe('[CaseFinancial::updateFee] function test', () => {
 });
 
 describe('[CaseFinancial::getAllFeeTemplates] function test', () => {
-  it('should resolve a collection of case fees', (done) => {
-    cw6.case.financial.getAllFeeTemplates().then(r => { // TODO: find case, then get fees
+  it('should resolve a collection of all fee templates', (done) => {
+    cw6.case.financial.getAllFeeTemplates().then(r => {
       assert.isArray(r);
       done();
     });
@@ -73,11 +73,100 @@ describe('[CaseFinancial::getAllFeeTemplates] function test', () => {
 
 describe('[CaseFinancial::searchFeeTemplates] function test', () => {
   it('should resolve a collection of case fees with interest in the name', (done) => {
-    cw6.case.financial.searchFeeTemplates({FeeDesc: 'interest'}).then(r => { // TODO: find case, then get fees
+    cw6.case.financial.searchFeeTemplates({FeeDesc: 'interest'}).then(r => {
       assert.isArray(r);
       done();
     }).catch(e => {
       console.log(e, 'e')
     })
   });
+});
+
+describe('[CaseFinancial::searchCaseInstruments] function test', () => {
+  it('should resolve a collection of case instruments with MO in the state code', (done) => {
+    cw6.case.financial.searchCaseInstruments({StateCode: 'MO'}).then(r => {
+      assert.isArray(r);
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+describe('[CaseFinancial::getInstrumentList] function test', () => {
+  it('should resolve a collection of instruments', (done) => {
+    cw6.case.financial.getInstrumentList().then(r => {
+      expect(r).to.satisfy(function (iLA) {
+          if (iLA===null || _.isArray(iLA)) {
+              return true;
+          } else {
+              return false;
+          }
+      });
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+
+describe('[CaseFinancial::addCaseInstrumentRelease] function test', () => {
+  it('should resolve an object with the new case instrument releases\' info');
+});
+
+describe('[CaseFinancial::deleteCaseInstrumentRelease] function test', () => {
+  it('should resolve an object with the deleted case instrument releases\' info');
+});
+
+describe('[CaseFinancial::searchCaseInstrumentReleases] function test', () => {
+  it('should resolve a collection of case instrument releases which meet the search critera', (done) => {
+    cw6.case.financial.searchCaseInstrumentReleases({PercentReleased: '100'}).then(r => {
+      assert.isArray(r);
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+describe('[CaseFinancial::fees] function test', () => {
+  it('should resolve a collection of all fees configured', (done) => {
+    cw6.case.financial.fees().then(r => {
+      assert.isArray(r);
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+describe('[CaseFinancial::searchAvailableFees] function test', () => {
+  it('should resolve an array of fee IDs which meet the specified search criteria', (done) => {
+    cw6.case.financial.searchAvailableFees({FeeCode: 'F-'}).then(r => {
+      assert.isArray(r);
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+describe('[CaseFinancial::getTenderTypes] function test', () => {
+  it('should resolve a collection of all tender types configured', (done) => {
+    cw6.case.financial.getTenderTypes().then(r => {
+      assert.isArray(r);
+      done();
+    }).catch(e => {
+      console.log(e, 'e')
+    })
+  });
+});
+
+describe('[CaseFinancial::addTenderType] function test', () => {
+  it('should resolve an object with the new tender type\'s info');
+});
+
+describe('[CaseFinancial::updateTenderType] function test', () => {
+  it('should resolve an object with the details of the updated tender type');
 });
