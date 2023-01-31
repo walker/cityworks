bbb8ad2092f6db3a0cee3a0d68957dbadcdce1af
Walker <3987+walker@users.noreply.github.com>
2023-01-30 19:54:26 -0600

    Update documentation comments properly

--- src/case_assets.ts
+++ src/case_assets.ts
@@ -19,10 +19,9 @@ export class CaseAssets {
    *
    * @category Case Assets
    * @param {number} caObjectId - The Case Object to attach the data group to.
-   * @param {number} caseDataGroupId - CaseDataGroupId as defined in CaseDataGroup admin.
-   * @param {string} groupCode - The Group Code.
-   * @param {Object} [options] - Options for CaseDataGroup including GroupDesc, GroupSum, and SumFlag
-   * @return {Object} Returns Promise that represents an object describing CaDataGroupItemBase.
+   * @param {boolean} updateXY - Whether or not to update the Case's X/Y values when attaching the asset. Defaults to true.
+   * @param {Object} [options] - Options for CaseDataGroup including Address, AddressId, AssetId, AssetObjectId, AssetType, CityName, CrossStreet, DateExpired, ExpiredFlag, Facility_Id, FeatureAssetId, FeatureClass, FeatureObjectId, LegacyId, Level_Id, LndObjectId, Location, MasterFlag, ObjectId, StateCode, StreetDirection, StreetFraction, StreetName, StreetNumber, StreetPostDir, StreetType, Suite, TileNo, UpdateCaseData, XCoord, YCoord, ZCoord, and ZipCode
+   * @return {Object} Returns Promise that represents an object describing CaAddress.
    */
    attach(caObjectId: number, updateXY: boolean = true, options?: {Address?: string, AddressId?: number, AssetId?: string, AssetObjectId?: number, AssetType?: string, CityName?: string, CrossStreet?: string, DateExpired?: string, ExpiredFlag?: string, Facility_Id?: string, FeatureAssetId?: string, FeatureClass?: string, FeatureObjectId?: number, LegacyId?: string, Level_Id?: string, LndObjectId?: number, Location?: string, MasterFlag?: string, ObjectId?: string, StateCode?: string, StreetDirection?: string, StreetFraction?: string, StreetName?: string, StreetNumber?: number, StreetPostDir?: string, StreetType?: string, Suite?: string, TileNo?: string, UpdateCaseData?: boolean, XCoord?: number, YCoord?: number, ZCoord?: number, ZipCode?: string}) {
     return new Promise((resolve, reject) => {
@@ -42,11 +41,11 @@ export class CaseAssets {
   }
 
   /**
-   * Dettach specific asset from case
+   * Detach specific asset from case
    *
    * @category Case Assets
    * @param {number} caObjectId - The Case Object to attach the data group to.
-   * @param {boolean} updateXY - The business case ID
+   * @param {boolean} updateXY - Whether or not to update the Case's X/Y values when detaching the assets. Defaults to true.
    * @return {Object} Returns Promise that represents a collection of the default CaDataGroupItemBases.
    */
   detach(caAddressId: number, updateXY: boolean = true) {
@@ -68,10 +67,10 @@ export class CaseAssets {
    *
    * @category Case Assets
    * @param {number} caObjectId - The Case Object to detach the assets from.
-   * @param {boolean} updateXY - The business case ID
+   * @param {boolean} updateXY - Whether or not to update the Case's X/Y values when detaching the asset(s). Defaults to false.
    * @return {Object} Returns Promise that represents a collection of detached CaAddressIds.
    */
-  detachAll(caObjectId: number, updateXY: boolean = true) {
+  detachAll(caObjectId: number, updateXY: boolean = false) {
     return new Promise((resolve, reject) => {
       var data = {
         CaObjectId: caObjectId,
@@ -90,7 +89,7 @@ export class CaseAssets {
    *
    * @category Case Assets
    * @param {number} caObjectId - The Case Object from which to get attached assets
-   * @return {Object} Returns Promise that represents a collection of the default CaDataGroupItemBases.
+   * @return {Object} Returns Promise that represents a collection of the default CaAddresses.
    */
   getForCase(caObjectId: number) {
     return new Promise((resolve, reject) => {
@@ -110,8 +109,8 @@ export class CaseAssets {
    * Search for Case Assets. Include one or more of the search fields. A logical 'and' operation is applied to muliple search fields
    *
    * @category Case Assets
-   * @param {Object} filters - The parameters to search by. (CaDataDetailId, CaDataListId, ListValue)
-   * @return {Object} Returns Promise that represents an Array of resulting CaDataListIds
+   * @param {Object} filters - The parameters to search by. (AssetType, CaAddressId, CaObjectId, CityName, ExpiredFlag, StateCode, StateName, StreetName, StreetNumber, Suite, ZipCode)
+   * @return {Object} Returns Promise that represents an Array of resulting CaAddresses
    */
    search(filters?: Object) {
     return new Promise((resolve, reject) => {
