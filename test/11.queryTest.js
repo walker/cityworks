914e257c42f3d68ae6e7bb97e4330cb82f95cd4c
Walker <3987+walker@users.noreply.github.com>
2023-10-29 13:54:44 -0500

    Stub query class test

---
+++ test/11.queryTest.js
@@ -0,0 +1,20 @@
+'use strict';
+require('dotenv').config();
+var chai = require('chai');
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const cw11 = require('../dist/index.js');
+cw11.Cityworks.configure(process.env.domain, {path: process.env.path});
+
+before(function(done) {
+  this.timeout(20000000);
+  cw11.Cityworks.authenticate(process.env.login, process.env.password).then(resp => {
+    done();
+  }).catch(e => {
+    console.log(e, 'unexpected error')
+    done();
+  });
+});
+
