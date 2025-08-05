8a1c12eb0d50bf6398ca86a21d68ad7e2531234c
Walker <3987+walker@users.noreply.github.com>
2025-08-04 21:10:40 -0500

    Flesh out people methods

--- src/case_people.ts
+++ src/case_people.ts
@@ -15,7 +15,7 @@ export class CasePeople {
   }
 
   /**
-   * 
+   * Add a people record to a case
    *
    * @category Case People
    * @param {number} caObjectId - The Case Object ID for the case to which to add the person
@@ -38,11 +38,121 @@ export class CasePeople {
     })
   }
 
-  //Pll/PeopleRole/All // gets all people roles available
-  //Pll/CasePeople/ByCaObjectId
-  //Pll/CasePeople/Delete
-  //Pll/CasePeople/DeleteByCaObjectId
-  //Pll/CasePeople/Search
-  //Pll/CasePeople/SearchObject
+  /**
+   * Get all the people roles available
+   *
+   * @category Case People
+   * @return {Object} Returns Promise that represents an collection of configured People Roles
+   */
+  getRoles() {
+    return new Promise((resolve, reject) => {
+      this.cw.runRequest('Pll/PeopleRole/All', {}).then(r => {
+        resolve(r.Value)
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }
 
+  /**
+   * Get people records attached to a specific case
+   *
+   * @category Case People
+   * @param {number} caObjectId - The Case Object ID for the case to which to add the person
+   * @return {Object} Returns Promise that represents an object describing the newly-added person. See /{subdirectory}/apidocs/#/data-type-info;dataType=CaPeopleItemBase
+   */
+  get(caObjectId: number) {
+    return new Promise((resolve, reject) => {
+      var data = {
+        CaObjectId: caObjectId,
+      }
+      this.cw.runRequest('Pll/CasePeople/ByCaObjectId', data).then(r => {
+        resolve(r.Value)
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }
+
+  /**
+   * Delete specific people records
+   *
+   * @category Case People
+   * @param {number} caPeopleId - The Case Object ID for the case to which to add the person
+   * @return {Object} Returns Promise that represents the result of the delete operation.
+   */
+  delete(caPeopleId: number) {
+    return new Promise((resolve, reject) => {
+      var data = {
+        CaPeopleId: caPeopleId,
+      }
+      this.cw.runRequest('Pll/CasePeople/Delete', data).then(r => {
+        resolve(r.Value)
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }
+
+  /**
+   * Delete all people records attached to a specific case
+   *
+   * @category Case People
+   * @param {number} caObjectId - The Case Object ID for the case to which to add the person
+   * @return {Object} Returns Promise that represents the result of the delete operation.
+   */
+  deleteAll(caObjectId: number) {
+    return new Promise((resolve, reject) => {
+      var data = {
+        CaObjectId: caObjectId,
+      }
+      this.cw.runRequest('Pll/CasePeople/DeleteByCaObjectId', data).then(r => {
+        resolve(r.Value)
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }
+
+  /**
+   * Search for people records
+   *
+   * @category Case People
+   * @param {number} searchOptions - Where you specify search options that assume a logical 'and' operation
+   * @param {boolean} idsOnly - If you only wants the IDs of the people records that match the search criteria, set this to true. Otherwise, set it to false.
+   * @return {Object} Returns Promise that represents an array of people record IDs that match the search criteria.
+   */
+  search(searchOptions: {
+    AddressLine1: string
+    AddressLine2: string
+    CaObjectId: number
+    CaPeopleId: number
+    CityName: string
+    CountryCode: string
+    CountryName: string
+    Email: string
+    Name: string
+    PhoneHome: string
+    PhoneMobile: string
+    PhoneWork: string
+    RoleCode: string
+    RoleDesc: string
+    RoleId: number
+    StateCode: string
+    StateName: string
+    ZipCode: string
+  }, idsOnly: boolean = false) {
+    return new Promise((resolve, reject) => {
+      var data = searchOptions
+      var runUrl: string = 'Pll/CasePeople/SearchObject'
+      if(idsOnly) {
+        runUrl = 'Pll/CasePeople/Search'
+      }
+      this.cw.runRequest(runUrl, data).then(r => {
+        resolve(r.Value)
+      }).catch(e => {
+        reject(e)
+      })
+    })
+  }
 }
